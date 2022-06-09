a90f6c0bd695e156db16b2b4a1125882a0aa2cff
Kurt Puican Salas <u202016643@upc.edu.pe>
2022-06-09 17:16:36 -0500

    feat(user): upload user queries

---
+++ src/user/application/dtos/queries/get-user.dto.ts
@@ -0,0 +1,9 @@
+export class GetUserDto {
+    public id: number;
+    public firstName: string;
+    public lastName: string;
+    public dni: string;
+    public email: string;
+    public password: string;
+   
+  }
