80387387870cef5861f6ff156f98e071449a77b1
Rafael Luyo <u201919295@upc.edu.pe>
2022-06-09 17:44:20 -0500

    feat(user): upload application mappers user

---
+++ src/user/application/mappers/user.mapper.ts
@@ -0,0 +1,26 @@
+import { User } from '../../domain/entities/user.entity';
+import { UserTypeORM } from '../../infrastructure/persistence/typeorm/entities/user.typeorm';
+import { UserIdTypeORM } from '../../infrastructure/persistence/typeorm/value-objects/user.id.typeorm';
+import { NameTypeORM } from '../../../common/infrastructure/persistence/typeorm/entities/name.typeorm';
+import { DniTypeORM } from '../../../common/infrastructure/persistence/typeorm/entities/dni.typeorm';
+import { EmailTypeORM } from '../../infrastructure/persistence/typeorm/value-objects/email.typeorm';
+import { PasswordTypeORM } from '../../infrastructure/persistence/typeorm/value-objects/password.typeorm';
+
+export class UserMapper {
+    public static toTypeORM(user: User): UserTypeORM {
+      const userTypeORM: UserTypeORM = new UserTypeORM();
+      userTypeORM.id = user.getId() != null ? user.getId().getValue() : 0;
+      userTypeORM.name = NameTypeORM.from(
+        user.getName().getFirstName(),
+        user.getName().getLastName(),
+      );
+      userTypeORM.dni = DniTypeORM.from(user.getDni().getValue());
+      userTypeORM.email = EmailTypeORM.from(
+        user.getEmail().getValue(),
+      );
+      userTypeORM.password = PasswordTypeORM.from(
+        user.getPassword().getValue(),
+      );
+      return userTypeORM;
+    }
+  }
