9cfa02ad308f019669ff823f53ef4769c3f06060
Rafael Luyo <u201919295@upc.edu.pe>
2022-06-10 02:35:43 -0500

    feat(user): upload coach domain value-objects

---
+++ src/coach/domain/value-objects/coach-id.value.ts
@@ -0,0 +1,15 @@
+export class CoachId {
+    private readonly value: number;
+  
+    private constructor(value: number) {
+      this.value = value;
+    }
+  
+    public static create(value: number) {
+      return new CoachId(value);
+    }
+  
+    public getValue(): number {
+      return this.value;
+    }
+  }
