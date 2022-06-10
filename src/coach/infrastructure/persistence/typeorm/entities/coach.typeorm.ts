c4b73b2035c32ab49862bec78937a699a35e1c2e
Kurt Puican Salas <u202016643@upc.edu.pe>
2022-06-10 01:58:11 -0500

    feat(coach): update coachts

---
+++ src/coach/infrastructure/persistence/typeorm/entities/coach.typeorm.ts
@@ -0,0 +1,27 @@
+import { Column, Entity, Unique } from 'typeorm';
+import { CoachIdTypeORM } from './coach.id.typeorm';
+import { NameCoachTypeORM } from './namecoach.typeorm';
+import { EmailTypeORM } from 'src/common/infrastructure/persistence/typeorm/entities/email.typeorm';
+import { PasswordTypeORM } from 'src/common/infrastructure/persistence/typeorm/entities/password.typeorm';
+
+@Entity('coach')
+@Unique('UQ_company', ['email.value'])
+export class CompanyTypeORM {
+  @Column((type) => CoachIdTypeORM, { prefix: false })
+  public id: CoachIdTypeORM;
+
+  @Column((type) => NameCoachTypeORM, { prefix: false })
+  public nameCompany: NameCoachTypeORM;
+
+  @Column((type) => EmailTypeORM, { prefix: false })
+  public email: EmailTypeORM;
+
+  @Column((type) => PasswordTypeORM, { prefix: false })
+  public password: PasswordTypeORM;
+
+  @Column('varchar', { name: 'description_coach', length: 500, nullable: false })
+  public descriptionCompany: string;
+
+  @Column('varchar', { name: 'img_coach', length: 500, nullable: false })
+  public imgCompany: string;
+}
