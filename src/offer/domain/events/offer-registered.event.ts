6540fba918aa4ac4597971e327212173ff18f5b7
estoico-exe <u20171d006@upc.edu.pe>
2022-06-10 02:41:49 -0500

    feat: implement offer registered event

---
+++ src/offer/domain/events/offer-registered.event.ts
@@ -0,0 +1,13 @@
+export class OfferRegisteredEvent{
+    constructor(
+    public id: number,
+    public title: string,
+    public description: string,
+    public pricePerIndividualSession: number,
+    public pricePerGroupSession: number,
+    public typeMoney: string,
+    public coachId: number,
+    public statusPublication: boolean,
+    public createdAt: string,
+    ){}
+}
