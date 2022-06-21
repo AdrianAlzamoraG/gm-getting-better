f16bb7cfe0f94f7c2fb48b1d9928b969f72d08c0
Kurt Puican Salas <u202016643@upc.edu.pe>
2022-06-21 14:36:24 -0500

    feat(coach): updated name

--- src/coach/application/commands/update-coach.command.ts
+++ src/coach/application/commands/update-coach.command.ts
@@ -2,7 +2,7 @@ export class UpdateCoachCommand {
     constructor(
       public readonly targetId: number,
       public readonly id: number,
-      public readonly nameCompany: string,
+      public readonly nameCoach: string,
       public readonly email: string,
       public readonly password: string,
       public readonly descriptionCoach: string,
