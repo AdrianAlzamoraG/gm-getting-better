c5bc16f1c7799616b7dabc22bb2f3fbd7762988e
Rafael Luyo <u201919295@upc.edu.pe>
2022-06-21 14:37:32 -0500

    feat(coach): upload coach application dtos queries get-coach-by-id

---
+++ src/coach/application/dtos/queries/get-coach-by-id.dto.ts
@@ -0,0 +1,8 @@
+export class GetCoachByIdDto {
+    public id: number;
+    public nameCoach: string;
+    public email: string;
+    public password: string;
+    public descriptionCoach: string;
+    public imgCoach: string;
+  }
