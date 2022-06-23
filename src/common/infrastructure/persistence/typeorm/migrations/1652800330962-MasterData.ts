a771744cced7db28589821a11f8102787b62a3a5
AdrianAlzamoraG <u202015385@upc.edu.pe>
2022-06-23 17:23:42 -0500

    feat(common): masterdata is implemented and formatting is applied to the code to clean it up

---
+++ src/common/infrastructure/persistence/typeorm/migrations/1652800330962-MasterData.ts
@@ -0,0 +1,15 @@
+import { SqlReader } from 'node-sql-reader';
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class MasterData1652800330962 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    const folder = __dirname;
+    const path = folder + '/master-data.sql';
+    const queries = SqlReader.readSqlFile(path);
+    for (const query of queries) {
+      await queryRunner.query(query);
+    }
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
