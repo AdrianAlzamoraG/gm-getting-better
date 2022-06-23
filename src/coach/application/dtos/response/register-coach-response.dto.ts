4e67493011451fe02791a385bbdc476ed4143c80
Rafael Luyo <u201919295@upc.edu.pe>
2022-06-23 16:23:19 -0500

    feat(coach): upload coach application dtos response

--- src/coach/application/dtos/response/register-coach-response.dto.ts
+++ src/coach/application/dtos/response/register-coach-response.dto.ts
@@ -1,10 +1,7 @@
-export class RegisterCoachResponseDto {
-    constructor(
-      public id: number,
-      public readonly nameCoach: string,
-      public readonly email: string,
-      public readonly password: string,
-      public readonly descriptionCoach: string,
-      public readonly imgCoach: string,
-    ) {}
-  }
+export class RegisterCoachResponse {
+  constructor(
+    public id: number,
+    public readonly name: string,
+    public readonly ruc: string
+  ) {}
+}
