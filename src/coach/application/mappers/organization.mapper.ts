4ca54ccef002bb3ec8012efc3ae6ae475b5c2def
Kurt Puican Salas <u202016643@upc.edu.pe>
2022-06-23 16:57:12 -0500

    feat(coach): upload mappers

---
+++ src/coach/application/mappers/organization.mapper.ts
@@ -0,0 +1,20 @@
+import { RucTypeORM } from '../../infrastructure/persistence/typeorm/value-objects/ruc.typeorm';
+import { Organization } from '../../domain/entities/organization.entity';
+import { OrganizationTypeORM } from '../../infrastructure/persistence/typeorm/entities/organization.typeorm';
+import { OrganizationNameTypeORM } from '../../infrastructure/persistence/typeorm/value-objects/organization-name.typeorm';
+import { AuditTrailTypeORM } from '../../../common/infrastructure/persistence/typeorm/value-objects/audit-trail.typeorm';
+
+export class OrganizationMapper {
+  public static toTypeORM(organization: Organization): OrganizationTypeORM {
+    const organizationTypeORM: OrganizationTypeORM = new OrganizationTypeORM();
+    organizationTypeORM.organizationName = OrganizationNameTypeORM.from(organization.getName().getValue());
+    organizationTypeORM.ruc = RucTypeORM.from(organization.getRuc().getValue());
+    const createdAt: string = organization.getAuditTrail() != null && organization.getAuditTrail().getCreatedAt() != null ? organization.getAuditTrail().getCreatedAt().format() : null;
+    const createdBy: number = organization.getAuditTrail() != null && organization.getAuditTrail().getCreatedBy() != null ? organization.getAuditTrail().getCreatedBy().getValue() : null;
+    const updatedAt: string = organization.getAuditTrail() != null && organization.getAuditTrail().getUpdatedAt() != null ? organization.getAuditTrail().getUpdatedAt().format() : null;
+    const updatedBy: number = organization.getAuditTrail() != null && organization.getAuditTrail().getUpdatedBy() != null ? organization.getAuditTrail().getUpdatedBy().getValue() : null;
+    const auditTrailTypeORM: AuditTrailTypeORM = AuditTrailTypeORM.from(createdAt, createdBy, updatedAt, updatedBy);
+    organizationTypeORM.auditTrail = auditTrailTypeORM;
+    return organizationTypeORM;
+  }
+}
