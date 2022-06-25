32f48c673dfd07bb249c6390989cde8cbaec9e1a
AdrianAlzamoraG <u202015385@upc.edu.pe>
2022-06-25 04:03:21 -0500

    feat(customization): customizations module was implemented

---
+++ src/customizations/customizations.module.ts
@@ -0,0 +1,44 @@
+import { Module } from '@nestjs/common';
+import { CqrsModule } from '@nestjs/cqrs';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { SaveCostHandler } from './application/handlers/commands/save-cost.handler';
+import { CustomizationsApplicationService } from './application/services/customizations-application.service';
+import { CustomizationsController } from './api/customizations.controller';
+import { SaveCostValidator } from './application/validators/save-cost-validator.service';
+/*
+import { WithdrawMoneyValidator } from './application/validators/withdraw-money.validator';
+import { TransferMoneyValidator } from './application/validators/transfer-money.validator';
+*/
+// import { WithdrawMoneyHandler } from './application/handlers/commands/withdraw-money.handler';
+// import { TransferMoneyHandler } from './application/handlers/commands/transfer-money.handler';
+import { CustomizationTypeorm } from './infrastructure/persistence/typeorm/entities/customization.typeorm';
+import { OfferTypeORM } from '../offers/infrastructure/persistence/typeorm/entities/offer.typeorm';
+import { CostSavedHandler } from './application/handlers/events/cost-saved.handler';
+import { CompleteCustomizationHandler } from './application/handlers/commands/complete-customization.handler';
+
+export const CommandHandlers = [
+  SaveCostHandler,
+  /*WithdrawMoneyHandler,
+  TransferMoneyHandler,*/
+  CompleteCustomizationHandler,
+];
+export const EventHandlers = [CostSavedHandler];
+export const QueryHandlers = [];
+
+@Module({
+  imports: [
+    CqrsModule,
+    TypeOrmModule.forFeature([CustomizationTypeorm, OfferTypeORM]),
+  ],
+  controllers: [CustomizationsController],
+  providers: [
+    CustomizationsApplicationService,
+    SaveCostValidator,
+    /* WithdrawMoneyValidator,
+    TransferMoneyValidator,*/
+    ...CommandHandlers,
+    ...EventHandlers,
+    ...QueryHandlers,
+  ],
+})
+export class CustomizationsModule {}
