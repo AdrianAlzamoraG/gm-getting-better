f2259c353590c14aa67f0bff7152b776dfadfd7f
Kurt Puican Salas <u202016643@upc.edu.pe>
2022-06-25 18:48:10 -0500

    feat(coach): upload prototype pattern

---
+++ src/coaches/prototype/coach-id.js
@@ -0,0 +1,10 @@
+"use strict";
+exports.__esModule = true;
+exports.CoachId = void 0;
+var CoachId = /** @class */ (function () {
+    function CoachId(id) {
+        this.id = id;
+    }
+    return CoachId;
+}());
+exports.CoachId = CoachId;
