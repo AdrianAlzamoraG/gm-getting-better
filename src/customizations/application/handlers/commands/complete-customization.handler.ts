26a777036346a03de92bc743e9fa8d523b527b58
AdrianAlzamoraG <u202015385@upc.edu.pe>
2022-06-25 03:53:06 -0500

    feat(customization): application handlers were implemented

---
+++ src/customizations/application/handlers/commands/complete-customization.handler.ts
@@ -0,0 +1,37 @@
+import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CustomizationTypeorm } from '../../../infrastructure/persistence/typeorm/entities/customization.typeorm';
+import { CustomizationStatus } from '../../../domain/enums/customization.status.enum';
+import { CompleteCustomization } from '../../commands/complete-customization.command';
+
+@CommandHandler(CompleteCustomization)
+export class CompleteCustomizationHandler
+  implements ICommandHandler<CompleteCustomization>
+{
+  constructor(
+    @InjectRepository(CustomizationTypeorm)
+    private customizationRepository: Repository<CustomizationTypeorm>,
+  ) {}
+
+  async execute(command: CompleteCustomization) {
+    const customizationId: number = command.customizationId;
+    let customizationTypeORM: CustomizationTypeorm =
+      await this.customizationRepository
+        .createQueryBuilder()
+        .where('id = :id')
+        .setParameter('id', customizationId)
+        .getOne();
+    if (customizationTypeORM == null) {
+      return false;
+    }
+    customizationTypeORM.status = CustomizationStatus.SAVED;
+    customizationTypeORM = await this.customizationRepository.save(
+      customizationTypeORM,
+    );
+    if (customizationTypeORM == null) {
+      return false;
+    }
+    return true;
+  }
+}
