8bf12d325c9fa3c8178827a613c0c5d33dcd42fc
AdrianAlzamoraG <u202015385@upc.edu.pe>
2022-06-25 02:40:19 -0500

    feat(customization): infraestructure entities for persistence were implemented

---
+++ src/customizations/infrastructure/persistence/typeorm/entities/customization.typeorm.ts
@@ -0,0 +1,39 @@
+import { Column, Entity, PrimaryGeneratedColumn, Unique } from 'typeorm';
+import { OfferIdFromTypeorm } from '../value-objects/offer-id-from.typeorm';
+import { OfferIdToTypeorm } from '../value-objects/offer-id-to.typeorm';
+import { AmountTypeORM } from '../value-objects/amount.typeorm';
+import { AuditTrailTypeORM } from '../../../../../common/infrastructure/persistence/typeorm/value-objects/audit-trail.typeorm';
+import { CustomizationStatus } from '../../../../domain/enums/customization.status.enum';
+
+@Entity('customizations')
+export class CustomizationTypeorm {
+  @PrimaryGeneratedColumn('increment', {
+    type: 'bigint',
+    name: 'id',
+    unsigned: true,
+  })
+  public id: number;
+
+  @Column('char', { name: 'type', length: 1, nullable: false })
+  public type: string;
+
+  @Column((type) => OfferIdFromTypeorm, { prefix: false })
+  public accountIdFrom: OfferIdFromTypeorm;
+
+  @Column((type) => OfferIdToTypeorm, { prefix: false })
+  public accountIdTo: OfferIdToTypeorm;
+
+  @Column((type) => AmountTypeORM, { prefix: false })
+  public amount: AmountTypeORM;
+
+  @Column('tinyint', {
+    name: 'status',
+    width: 2,
+    unsigned: true,
+    nullable: false,
+  })
+  public status: CustomizationStatus;
+
+  @Column((type) => AuditTrailTypeORM, { prefix: false })
+  public auditTrail: AuditTrailTypeORM;
+}
