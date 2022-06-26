776838626275c4d123e56816253b48cddb60811e
AdrianAlzamoraG <u202015385@upc.edu.pe>
2022-06-25 20:08:11 -0500

    feat(swagger): implemented getting better API documentation using swagger technology

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   console.log(__dirname);
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('GETTING BETTER API')
+    .setDescription('GETTING BETTER API documentation')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(8080);
 }
 bootstrap();
