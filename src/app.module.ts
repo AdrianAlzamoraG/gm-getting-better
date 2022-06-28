ba71b40fbbf3c6419b9b21c99f957a200c3101ff
estoico-exe <u20171d006@upc.edu.pe>
2022-06-27 19:17:22 -0500

    chore: update files to deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,10 +9,30 @@ import { CustomizationsModule } from './customizations/customizations.module';
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: "mysql",
+      url: process.env.GB_DDD_NEST_MYSQL,
+      migrationsRun: true,
+      logging: true,
+      timezone: '+00:00',
+      bigNumberStrings: false,
+      entities: [
+        process.env.ENVIRONMENT == 'prod' ?
+          '**/infrastructure/persistence/typeorm/entities/*{.ts,.js}' :
+          'dist/**/infrastructure/persistence/typeorm/entities/*{.ts,.js}'
+      ],
+      subscribers: [],
+      migrations: [
+        process.env.ENVIRONMENT == 'prod' ?
+          'common/infrastructure/persistence/typeorm/migrations/*{.ts,.js}' :
+          'dist/common/infrastructure/persistence/typeorm/migrations/*{.ts,.js}'
+      ],
+      migrationsTableName: "migrations"
+    }),
     CoachesModule,
     OffersModule,
     CustomizationsModule,
-    TypeOrmModule.forRoot(),
+
   ],
   controllers: [AppController],
   providers: [AppService],
